c002c0433b611a0abd5425e24b572f709b796c23
mohamedsherif301 <myosre122@gmail.com>
2025-06-22 16:39:08 +0300

    Add Blur '25PX' in Model form (ADD, UpDate )& Remove Product

--- src/Components/UI/Model.tsx
+++ src/Components/UI/Model.tsx
@@ -1,4 +1,4 @@
-import { Dialog, Transition, TransitionChild } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, type ReactNode } from "react";
 
 interface IProps {
@@ -14,9 +14,8 @@ const Modal = ({ isOpen, closeModal, title, description, children }: IProps) => 
     <>
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          {/* backdrop-blur-sm */}
-          <div className="fixed inset-0 " aria-hidden="true" />
-          <TransitionChild
+          <div className="fixed inset-0 backdrop-blur-sm" aria-hidden="true" />
+          <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
             enterFrom="opacity-0"
@@ -25,9 +24,8 @@ const Modal = ({ isOpen, closeModal, title, description, children }: IProps) => 
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            {/* bg-opacity-210 */}
-            <div className="fixed inset-0  " />
-          </TransitionChild>
+            <div className="fixed inset-0 " />
+          </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center p-4 text-center">
